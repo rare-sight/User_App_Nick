aa116ac83e7bc23041bd898b533b65bc75a185f5
rare-sight <nikhilpanchal2724@gmail.com>
2025-09-18 16:56:51 +0530

    updated version

--- userapp/lib/pages/home.dart
+++ userapp/lib/pages/home.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -8,13 +8,49 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Prototype Home',
+        title: const Text(
+           'Home',
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 24,
             color: Colors.black,
-          ),),
+          ),
+        ),
         centerTitle: true,
+      leading: Container(
+        alignment: Alignment.center,
+          height: 20,
+          width: 20,
+          margin: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+
+            borderRadius: BorderRadius.circular(8),
+            color: const Color.fromARGB(255, 248, 246, 246),
+            
+          ),
+          child: SvgPicture.asset('assets/icons/threedotshorizontal.svg'),
+          
+      ),
+      actions: [Container(
+          alignment: Alignment.center,
+          height: 40,
+          width: 40,
+          margin: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+
+            borderRadius: BorderRadius.circular(8),
+            color: const Color.fromARGB(255, 248, 246, 246),
+            
+          ),
+          child: SvgPicture.asset('assets/icons/profile.svg'),
+          
+      ),],
+        /*leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.black),
+          onPressed: () {},
+        ),*/
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        elevation: 0,
       ),
     );
   }
